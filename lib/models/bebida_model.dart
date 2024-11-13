02117086cd6eed8e1445e558e7736db594f9002e
MariV23 <124789760+MariV23@users.noreply.github.com>
2024-11-13 01:29:03 -0600

    bebidas

--- lib/models/bebida_model.dart
+++ lib/models/bebida_model.dart
@@ -3,19 +3,22 @@ class Bebida{
   String? nombre;
   double? precio;
   int? categoriaId;
+  int? stock;
 
   Bebida({
         this.idBebida,
         this.nombre,
         this.precio,
-        this.categoriaId
+        this.categoriaId,
+        this.stock
       });
 
   factory Bebida.fromJson(Map<String, dynamic> json) => Bebida(
     idBebida: json['BebidaId'] as int,
     nombre: json['Nombre'],
-        precio: json['precio'],
+        precio: json['precio'] as double,
         categoriaId: json['categoriaId']as int,
+        stock: json['stock'] as int
       );
 
 }
--- lib/screens/bebidas.dart
+++ lib/screens/bebidas.dart
@@ -1,26 +1,46 @@
-/*import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+import 'package:mydrink_app/models/categoria_model.dart';
+import 'package:mydrink_app/screens/home_screen.dart';
+import 'package:mydrink_app/services/categorias_service.dart';
+import 'package:mydrink_app/providers/user_provider.dart';
+import 'package:provider/provider.dart';
+import 'package:mydrink_app/models/bebida_model.dart';
+
 class BebidasScreen extends StatefulWidget{
   const BebidasScreen({super.key});
-
   @override
+  State<BebidasScreen>  createState() => _bebidasScreenState();
+}
+  class _bebidasScreenState extends State<BebidasScreen>{
+  TextEditingController nombreController = TextEditingController();
+  TextEditingController precioController = TextEditingController();
+  TextEditingController CatIDController = TextEditingController();
+  TextEditingController stockController = TextEditingController();
+
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    double width = size.width;
-    return Scanffold(
-      appBar:AppBar(
-        title:const Text("Bebidas"),
-      ),
-      body:FutureBuilder<Bebidas>(
-        future:fetchBebidas(),
-        builder:(context,snapshot){
-          if(snapshot.connectionState==ConnectionState.waiting){
-            return const Center(child:
-            CircularProgressIndicator());
-          }else if(snapshot.hasError){
-            return Center(child:Text("Error: ${snapshot.error}"))
+      Size size = MediaQuery.of(context).size;
+      double width = size.width;
+      return Scanffold(
+        appBar:AppBar(
+          title:const Text("Bebidas"),
+        ),
+        body:FutureBuilder<Bebida>(
+          future:fetchBebidas(),
+          builder:(context,snapshot){
+            if(snapshot.connectionState==ConnectionState.waiting){
+              return const Center(child:
+              CircularProgressIndicator());
+            }else if(snapshot.hasError){
+              return Center(child:Text("Error: ${snapshot.error}"))
+            }
           }
-        }
+        )
       )
-    )
+    }
+
+
   }
-}*/
+
+ 
+}
