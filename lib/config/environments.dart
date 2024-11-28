e0cc01406c44f07bb8e8f438355ce335f63aed63
Hector.Castellanos <hectorcastellanos429@gmail.com>
2024-11-27 19:39:07 -0600

    update other APIS

--- lib/config/environments.dart
+++ lib/config/environments.dart
@@ -1,8 +1,8 @@
 class Environments {
   static bool demo = false;
 
-  static String apiURl = 'http://192.168.1.168:3000/api';
+  static String apiURl = 'http://172.31.18.10:3000/api';
 
-  static String direccionServer = 'http://192.168.1.168:3000';
+  static String direccionServer = 'http://172.31.18.10:3000';
   static String direccionUser = 'http://192.168.1.161';
 }
