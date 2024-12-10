09f6015b8079c0ba9c7cdbab4d89d71fb26841af
HectorCastellanosVV <hectorcastellanos429@gmail.com>
2024-12-09 18:37:17 -0600

    update 3.1

--- lib/services/user_services.dart
+++ lib/services/user_services.dart
@@ -1,23 +1,35 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:mydrink_app/config/environments.dart';
 import 'package:mydrink_app/models/user_model.dart';
 import 'package:mydrink_app/providers/user_provider.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class UserServices {
   Future<User?> login(
       {required String username, required String password}) async {
     try {
+      final key = encrypt.Key.fromUtf8('32charlongencryptionkey32');
+      final iv = encrypt.IV.fromLength(16);
+
+      final encrypter = encrypt.Encrypter(encrypt.AES(key));
+
+      final encryptedPassword = encrypter.encrypt(password, iv: iv);
+
       var headers = {
         'Content-Type': 'application/json',
         'Origin': Environments.direccionUser
       };
 
       var response = await http.post(
-          Uri.parse('${Environments.direccionServer}/api/login'),
-          headers: (headers),
-          body: json.encode({"username": username, "password": password}));
+        Uri.parse('${Environments.direccionServer}/api/login'),
+        headers: (headers),
+        body: json.encode({
+          "username": username,
+          "password": encryptedPassword
+              .base64, // Envía la contraseña encriptada en base64
+        }),
+      );
 
       if (response.statusCode == 200) {
         final authResponse = jsonDecode(response.body);
@@ -28,7 +40,7 @@ class UserServices {
         return null;
       }
     } catch (e) {
-      print('Error $e');
+      //print('Error $e');
       return null;
     }
   }
